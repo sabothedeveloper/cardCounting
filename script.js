7a2ada9c6c2638778953eb38b3c7ad6e0703488f
Szabolcs Nagy <109295080+szabolcsthedeveloper@users.noreply.github.com>
2023-07-03 17:16:56 +0200

    create inital script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// FIRST ITERATION
+
+let count = 0;
+
+function cc(card) {
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+     count++;
+     break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+     count--;
+     break;
+  }
+
+  let holdbet = "Hold"
+  if (count > 0) {
+    holdbet = "Bet"
+  }
+
+  return count + " " + holdbet;
+}
